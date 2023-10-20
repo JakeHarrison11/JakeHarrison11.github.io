9fb609fe91b7f348ca7a8956370e1bc0448cc40c
Jake Harrison <jake@Jakes-MacBook-Air.local>
2023-10-19 20:06:12 -0400

    initial commit

---
+++ notion2website.sh
@@ -0,0 +1,4 @@
+unzip dd734beb-6e4c-45e6-bacf-6150d65d4bd1_Export-e38dd269-ce21-4bb3-8192-90348079a628.zip 
+rm dd734beb-6e4c-45e6-bacf-6150d65d4bd1_Export-e38dd269-ce21-4bb3-8192-90348079a628.zip
+mv "CTF 4554b692b27c40e08d1bc1bc51772fcf.html" index.html
+
